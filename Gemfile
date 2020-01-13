1acb8dcf8a799527c435d974a269501ecf767bc1
Dane O'Connor <dane.oconnor@gmail.com>
2020-01-13 00:08:42 -0500

    Add en/es locales

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'i18n'
+gem 'rack-contrib'
--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require 'sinatra'
+require 'i18n'
+require 'i18n/backend/fallbacks'
+
+configure do
+  I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+  I18n.load_path += Dir[File.join(settings.root, 'locales', '*.yml')]
+  I18n.backend.load_translations
+end
+
+before "/:locale*" do
+  locale = params[:locale].to_sym
+  halt 404 unless I18n.available_locales.include?(locale)
+  I18n.locale = locale
+  request.path_info = '/' + params[:splat ][0]
+end
 
 get '/' do
   erb :index
