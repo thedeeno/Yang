d97b9784844c4550c9369636aacad1550a2576db
Dane O'Connor <dane.oconnor@gmail.com>
2020-01-28 18:59:22 -0500

    Handle social agents

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ def accepted_locales(http_accept_language = request.env['HTTP_ACCEPT_LANGUAGE'])
   #
   # https://gist.github.com/naomik/5546046
   #
-  return [] if http_accept_language&.empty?
+  return [] if http_accept_language.nil? || http_accept_language.empty?
   langs = http_accept_language.scan(/([a-zA-Z]{2,4})(?:-[a-zA-Z]{2})?(?:;q=(1|0?\.[0-9]{1,3}))?/).map do |pair|
     lang, q = pair
     [lang.to_sym, (q || '1').to_f]
